993e059f51913863cc7666344f02d7065ad77495
Mariana Savoldi <marianasavodi123@gmail.com>
2021-04-26 20:58:53 -0300

    FInalizando o exercício de fixação

---
+++ fix-app/src/Component.js
@@ -0,0 +1,12 @@
+function Component() {
+  return (
+  <div className="text-test">
+    <h1>Mariana Savoldi Pereira</h1>
+      <p>Olá! Pode me chamar de Mari 😄 <br/>
+      Tenho 20 anos e atualmente estou iniciando o módulo de Front-end no curso da Trybe.
+      </p>
+  </div>
+  )
+}
+
+export default Component;
