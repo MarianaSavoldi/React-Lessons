085825d19e87c78187a44aec33a4d19c47810f3f
Mariana Savoldi <marianasavodi123@gmail.com>
2021-05-14 17:44:52 -0300

    Finalizando fixaçao 11.2

---
+++ fixation-exercises-11-2/src/App.js
@@ -0,0 +1,10 @@
+import Image from './Image.js'
+import './App.css';
+
+function App() {
+  return (
+    <Image source = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText = 'Cute cat' />
+  );
+}
+
+export default App;
